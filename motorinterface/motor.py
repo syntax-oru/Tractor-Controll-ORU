8bc327daacfe0572c722838c1b8d416d92261152
Hampus Siik <112425988+TheLuckyLuke1337@users.noreply.github.com>
2024-04-30 16:54:19 +0200

    Add Motor and IdealSimulatedMotor classes to motor.py

---
+++ motorinterface/motor.py
@@ -0,0 +1,90 @@
+try:
+    from utime import ticks_us
+except ImportError:
+    from time import ticks_us
+from math import pi
+
+
+class Motor:
+    def __init__(self) -> None:
+        ...
+
+    @property
+    def speed(self) -> float:
+        ...
+
+    @speed.setter
+    def speed(self, value: float) -> None:
+        ...
+
+    @property
+    def position(self) -> float:
+        ...
+
+    @position.setter
+    def position(self, value: float) -> None:
+        ...
+
+    def start(self) -> None:
+        ...
+
+    def stop(self) -> None:
+        ...
+
+    @property
+    def enabled(self) -> bool:
+        ...
+
+    @enabled.setter
+    def enabled(self, value: bool) -> None:
+        ...
+
+
+class IdealSimulatedMotor(Motor):
+    def __init__(self) -> None:
+        self._speed = 0.0
+        self._position = 0.0
+        self._on = False
+        self._previous_time = ticks_us()
+
+    def _update_position(self) -> None:
+        current_time = ticks_us()
+        elapsed_time = current_time - self._previous_time
+        self._position += (self._speed * elapsed_time) % (2 * pi)
+        self._previous_time = current_time
+
+    @property
+    def speed(self) -> float:
+        return self._speed
+
+    @speed.setter
+    def speed(self, value: float) -> None:
+        self._update_position()
+        self._speed = value
+
+    @property
+    def position(self) -> float:
+        self._update_position()
+        return self._position
+
+    @position.setter
+    def position(self, value: float) -> None:
+        self._position = value
+
+    @property
+    def enabled(self) -> bool:
+        return self._on
+
+    @enabled.setter
+    def enabled(self, value: bool) -> None:
+        if value is True:
+            self.start()
+        else:
+            self.stop()
+
+    def start(self) -> None:
+        self._on = True
+
+    def stop(self) -> None:
+        self._on = False
+        self.speed = 0.0
